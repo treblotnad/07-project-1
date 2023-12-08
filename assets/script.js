f0ccb004d4ba1657bb949f7d105ab7e82d859066
treblot <treblotnad@gmail.com>
2023-12-07 20:27:41 -0600

    added second API for bird search by lat,long from park api

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,35 @@
 var searchBtn = document.getElementById("searchBtn");
 var searchText = document.getElementById("searchText");
+var parkInfo = document.getElementById("parkInfo");
 var requestUrl = "https://developer.nps.gov/api/v1/parks?limit=500&api_key=";
-var apiKey = "Y4jn9Z8wuXTpAzOWaleVwq7CizZECJdzBZfX1a2Y";
+var apiKey = "apiKeyPark";
 
+var myHeaders = new Headers();
+myHeaders.append("X-eBirdApiToken", "apikeyBird");
+
+function searchBirds(lat, lng) {
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+    dist: 25,
+    back: 14,
+    sort: "species",
+  };
+  fetch(
+    "https://api.ebird.org/v2/data/obs/geo/recent?" +
+      "lat=" +
+      lat +
+      "&lng=" +
+      lng,
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then(function (result) {
+      console.log(JSON.parse(result));
+    })
+    .catch((error) => console.log("error", error));
+}
 function searchPark(parkName) {
   fetch(requestUrl + apiKey)
     .then(function (response) {
@@ -15,23 +42,23 @@ function searchPark(parkName) {
 }
 
 function filterParks(data, parkName) {
-  //   console.log(data.data[465].fullName);
-  //   console.log(data.data[465].fullName.toLowerCase().includes(parkName));
   for (let i = 0; i < data.data.length; i++) {
-    // console.log(data.data[i].fullName);
-    // console.log(parkName);
     if (data.data[i].fullName.toLowerCase().includes(parkName)) {
       console.log(data.data[i].fullName);
       var parkIndex = i;
       console.log(parkIndex);
     }
   }
-  //   console.log(parkIndex);
-  //   console.log(data.data[parkIndex].fullName);
-  //   return parkIndex;
+  parkInfo.textContent = data.data[parkIndex].fullName;
+  console.log(data.data[parkIndex]);
+  var parkLatitude = data.data[parkIndex].latitude;
+  var parkLongitude = data.data[parkIndex].longitude;
+  searchBirds(parkLatitude, parkLongitude);
 }
 
 searchBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  searchPark(searchText.value);
+  if (searchText.value.length < 3) {
+  }
+  searchPark(searchText.value.toLowerCase());
 });
