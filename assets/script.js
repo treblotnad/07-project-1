712113282e6abb1002427d9660a19136780232b3
treblot <treblotnad@gmail.com>
2023-12-14 15:20:39 -0600

    seperated out js files for both htmls. Deleted redundant js

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,13 @@
 var searchBtn = document.getElementById("searchBtn");
 var searchText = document.getElementById("search-input");
-var parkInfo = document.getElementById("parkInfo");
 var modalParkPicker = document.getElementById("modalParkPicker");
 var modalContent = document.getElementById("modalContent");
-var searchHistory = document.getElementById("searchHistory");
-var clearHistoryBtn = document.getElementById("clearHistory");
-var birdCards = document.getElementById("birdCards");
 var requestUrl = "https://developer.nps.gov/api/v1/parks?limit=500&api_key=";
 
 var apiKey = "Y4jn9Z8wuXTpAzOWaleVwq7CizZECJdzBZfX1a2Y";
 var parkData = "";
 var parkArray = [];
-var myHeaders = new Headers();
-myHeaders.append("X-eBirdApiToken", "144remuqd7dn");
 
-function searchBirds(lat, lng) {
-  var requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
-    dist: 25,
-    back: "1",
-    sort: "species",
-  };
-  fetch(
-    "https://api.ebird.org/v2/data/obs/geo/recent?" +
-      "lat=" +
-      lat +
-      "&lng=" +
-      lng,
-    requestOptions
-  )
-    .then((response) => response.text())
-    .then(function (result) {
-      console.log(JSON.parse(result));
-      renderBirds(JSON.parse(result));
-    })
-    .catch((error) => console.log("error", error));
-}
 function searchPark(parkName) {
   parkArray = [];
   fetch(requestUrl + apiKey)
@@ -106,48 +76,6 @@ function createModalButtons(parkArray) {
   }
 }
 
-function pickPark(pickedPark) {
-  for (let i = 0; i < parkArray.length; i++) {
-    if (pickedPark == parkArray[i].parkName) {
-      var parkLatitude = parkData.data[parkArray[i].parkArrayIndex].latitude;
-      var parkLongitude = parkData.data[parkArray[i].parkArrayIndex].longitude;
-      var parkWeb = parkData.data[parkArray[i].parkArrayIndex].url
-      var parkImage =parkData.data[parkArray[i].parkArrayIndex].images[0].url
-      var title = parkData.data[parkArray[i].parkArrayIndex].images[0].title
-      var credit = parkData.data[parkArray[i].parkArrayIndex].images[0].credit
-      var parkDescription =parkData.data[parkArray[i].parkArrayIndex].description
-      var parkEmail= parkData.data[parkArray[i].parkArrayIndex].contacts.emailAddresses[0].emailAddress
-      searchBirds(parkLatitude, parkLongitude);
-      console.log(parkData.data[parkArray[i].parkArrayIndex])
-      console.log(parkData.data[parkArray[i].parkArrayIndex].contacts.emailAddresses[0].emailAddress)
-      hero(pickedPark,parkWeb,parkImage,title,credit,parkDescription,parkEmail);
-    }
-  }
-  renderHistory();
-}
-
-var heroParkName = document.getElementById("fullName")
-var heroParkImage =document.getElementById("parkImage")
-var parkLink = document.getElementById("parkLink")
-var imageTitle = document.getElementById("imageTitle")
-var imagesCredit =document.getElementById("credit")
-var description = document.getElementById("descriptionOfPark")
-var parkInfo = document.getElementById("parkInfo")
-
-function hero(parkName,parkWeb,parkImage,title,credit,parkDescription,parkEmail){
-  heroParkName.textContent=parkName.toUpperCase()
-  parkLink.setAttribute("href", parkWeb); 
-  heroParkImage.style.backgroundImage= `url(${parkImage})`
-  heroParkImage.style.backgroundSize = "cover"
-  heroParkImage.style.backgroundRepeat ="no-repeat"
-  imageTitle.textContent = title
-  imagesCredit.textContent ="Photo by:"+ credit
-  description.textContent=parkDescription
-  parkInfo.textContent="Email us @:"+parkEmail
-  // park hours
-  // park 
-}
-
 searchBtn.addEventListener("click", function (e) {
   e.preventDefault();
   if (searchText.value.length < 3) {
@@ -164,12 +92,11 @@ modalContent.addEventListener("click", function (e) {
     if (e.target.textContent == "No park was found, please try again") {
       return;
     }
+    localStorage.clear();
     addSearchHistory(e.target.textContent);
     if (document.location.pathname == "/index.html") {
       document.location = "./results.html";
     }
-
-    pickPark(e.target.textContent.toLowerCase());
   }
 });
 
@@ -181,117 +108,4 @@ function addSearchHistory(parkSearched) {
   }
 
   localStorage.setItem(localStorage.length, parkSearched);
-}
-
-function renderHistory() {
-  while (searchHistory.firstChild) {
-    searchHistory.removeChild(searchHistory.firstChild);
-  }
-
-  for (
-    let i = localStorage.length - 1;
-    i > -1;
-    //i needs to be greater than -1 or greater than localStorage.length -1
-    i--
-  ) {
-    var historyBtn = document.createElement("button");
-    historyBtn.textContent = localStorage[i];
-    historyBtn.classList.add(
-      "button",
-      "is-large",
-      "tile",
-      "is-child",
-      "history",
-      "is-size-7"
-    );
-    searchHistory.appendChild(historyBtn);
-  }
-}
-
-if (document.location.pathname == "/results.html") {
-  renderHistory();
-  if (localStorage.length > 0) {
-    searchPark(localStorage.getItem(localStorage.length - 1).toLowerCase());
-  }
-}
-
-searchHistory.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (!e.target.classList.contains("history")) {
-    return;
-  }
-  var clickedBtn = e.target.textContent;
-  searchPark(clickedBtn);
-});
-
-clearHistoryBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  localStorage.clear();
-  renderHistory();
-});
-
-if (document.location.pathname == "/index.html") {
-  localStorage.clear();
-}
-
-function renderBirds(birdArray) {
-  while (birdCards.firstChild) {
-    birdCards.removeChild(birdCards.firstChild);
-  }
-  var birdNum = birdArray.length;
-  var birdDisplayCount = 24;
-  var birdIndex = 0;
-  if (birdNum < 24) {
-    birdDisplayCount = birdNum;
-  }
-
-  for (let i = 0; i < birdDisplayCount; i++) {
-    var cardBank = document.createElement("div");
-    for (let j = 0; j < 3; j++) {
-      var comName = birdArray[birdIndex].comName;
-      var locationSeen = birdArray[birdIndex].locName;
-      var obsDate = birdArray[birdIndex].obsDt;
-      var sciName = birdArray[birdIndex].sciName;
-      var birdCount = birdArray[birdIndex].howMany;
-
-      var cardToAdd = document.createElement("a");
-      var cardContent = document.createElement("div");
-      var mediaContent = document.createElement("div");
-      var nameContent = document.createElement("p");
-      var obsDateContent = document.createElement("p");
-      var sciNameContent = document.createElement("p");
-
-      cardBank.classList.add("columns", "is-centered");
-      cardToAdd.classList.add("card", "column", "is-4");
-      cardContent.classList.add("card-content");
-      mediaContent.classList.add("media-content");
-      nameContent.classList.add("title", "is-4");
-      sciNameContent.classList.add("subtitle", "is-6");
-      obsDateContent.classList.add("content");
-      cardToAdd.setAttribute(
-        "href",
-        "https://www.google.com/search?tbm=isch&q=" + comName
-      );
-      cardToAdd.setAttribute("target", "_blank");
-
-      nameContent.textContent = comName;
-      sciNameContent.textContent = sciName;
-      obsDateContent.textContent =
-        birdCount + " last Seen: " + obsDate + " at " + locationSeen;
-
-      cardBank.appendChild(cardToAdd);
-      cardToAdd.appendChild(cardContent);
-      cardContent.appendChild(mediaContent);
-      mediaContent.appendChild(nameContent);
-      mediaContent.appendChild(sciNameContent);
-      mediaContent.appendChild(obsDateContent);
-
-      if (birdIndex == birdDisplayCount - 1) {
-        break;
-      }
-      birdIndex++;
-    }
-    i = i + 2;
-    birdCards.appendChild(cardBank);
-  }
 }
