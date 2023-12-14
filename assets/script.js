75430b69fcf336a9726903c7f1fcdd2ec7153008
treblot <treblotnad@gmail.com>
2023-12-13 21:11:22 -0600

    Fixed search issue after previous merge, added bird card render function

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
 var searchBtn = document.getElementById("searchBtn");
-var searchText = document.getElementById("searchText");
+var searchText = document.getElementById("search-input");
 var parkInfo = document.getElementById("parkInfo");
 var modalParkPicker = document.getElementById("modalParkPicker");
 var modalContent = document.getElementById("modalContent");
 var searchHistory = document.getElementById("searchHistory");
+var clearHistoryBtn = document.getElementById("clearHistory");
+var birdCards = document.getElementById("birdCards");
 var requestUrl = "https://developer.nps.gov/api/v1/parks?limit=500&api_key=";
 
 var apiKey = "Y4jn9Z8wuXTpAzOWaleVwq7CizZECJdzBZfX1a2Y";
@@ -32,6 +34,7 @@ function searchBirds(lat, lng) {
     .then((response) => response.text())
     .then(function (result) {
       console.log(JSON.parse(result));
+      renderBirds(JSON.parse(result));
     })
     .catch((error) => console.log("error", error));
 }
@@ -176,7 +179,6 @@ function renderHistory() {
 if (document.location.pathname == "/results.html") {
   renderHistory();
   if (localStorage.length > 0) {
-    console.log(localStorage.getItem(localStorage.length - 1).toLowerCase());
     searchPark(localStorage.getItem(localStorage.length - 1).toLowerCase());
   }
 }
@@ -189,3 +191,70 @@ searchHistory.addEventListener("click", function (e) {
   var clickedBtn = e.target.textContent;
   searchPark(clickedBtn);
 });
+
+clearHistoryBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  localStorage.clear();
+  renderHistory();
+});
+
+if (document.location.pathname == "/index.html") {
+  localStorage.clear();
+}
+
+function renderBirds(birdArray) {
+  while (birdCards.firstChild) {
+    birdCards.removeChild(birdCards.firstChild);
+  }
+  var birdNum = birdArray.length;
+  var birdDisplayCount = 24;
+  if (birdNum < 24) {
+    birdDisplayCount = birdNum;
+  }
+
+  for (let i = 0; i < birdDisplayCount; i++) {
+    var cardBank = document.createElement("div");
+    for (let j = 0; j < 3; j++) {
+      var comName = birdArray[i].comName;
+      var locationSeen = birdArray[i].locName;
+      var obsDate = birdArray[i].obsDt;
+      var sciName = birdArray[i].sciName;
+      var birdCount = birdArray[i].howMany;
+
+      var cardToAdd = document.createElement("a");
+      var cardContent = document.createElement("div");
+      var mediaContent = document.createElement("div");
+      var nameContent = document.createElement("p");
+      var obsDateContent = document.createElement("p");
+      var sciNameContent = document.createElement("p");
+
+      cardBank.classList.add("columns", "is-centered");
+      cardToAdd.classList.add("card", "column", "is-4");
+      cardContent.classList.add("card-content");
+      mediaContent.classList.add("media-content");
+      nameContent.classList.add("title", "is-4");
+      sciNameContent.classList.add("subtitle", "is-6");
+      obsDateContent.classList.add("content");
+      cardToAdd.setAttribute(
+        "href",
+        "https://www.google.com/search?tbm=isch&q=" + comName
+      );
+      cardToAdd.setAttribute("target", "_blank");
+
+      nameContent.textContent = comName;
+      sciNameContent.textContent = sciName;
+      obsDateContent.textContent =
+        birdCount + " last Seen: " + obsDate + " at " + locationSeen;
+
+      cardBank.appendChild(cardToAdd);
+      cardToAdd.appendChild(cardContent);
+      cardContent.appendChild(mediaContent);
+      mediaContent.appendChild(nameContent);
+      mediaContent.appendChild(sciNameContent);
+      mediaContent.appendChild(obsDateContent);
+
+      i++;
+    }
+    birdCards.appendChild(cardBank);
+  }
+}
